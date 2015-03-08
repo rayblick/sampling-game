67a5e5cebe8227d2b20ecaa4f0b72582bcf31b2d
Ray Blick <rblick.ecol@gmail.com>
2015-03-08 18:20:34 +1000

    Added histogram to GUI and improved button states
    The histogram was added but it requires some work. It currently plots over itself after every game and it needs axis titles etc. Button states were improved so that the user could not have a second attempt after the answer was revealed. Also the return key has been bound to the entry field as a second option to the submit button.

--- sample_size.py
+++ sample_size.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 18 13:24:25 2015
-Last modified on Sunday Feb 8 08:51:00 2015
+Last modified on Sunday Feb 8 18:13:00 2015
 @author: uqrblick
 """
 from tkinter import *
@@ -18,6 +18,7 @@ class guessN:
 
     def __init__(self, master):
         self.Guess_ID = 0
+        self.error_data = [0,1]
         self.guess = []
         self.calculated_N = []
         
@@ -29,54 +30,59 @@ class guessN:
         self.style.configure('TLabel', font = ('Arial',11))
         self.style.configure('Header.TLabel',font = ('Arial', 20, 'bold'))
       
-        
         # build the application header
         self.img = PhotoImage(file = "images//logo.gif").subsample(5, 5)
-        ttk.Label(self.frame_header, image=self.img, anchor=CENTER).grid(row=0, column=0, rowspan=2, padx=10, pady=10)
-        ttk.Label(self.frame_header, text = "Welcome to guessN!", style='Header.TLabel').grid(row=0,column=1, padx=10, pady=10)
-        ttk.Label(self.frame_header, wraplength=600, text = ("Evaluate your own skill in guessing how many samples " 
-                                                    "you need to record a loss in a population...")).grid(row=1, column=1, padx=10)      
-        
+        ttk.Label(self.frame_header, image = self.img, anchor = CENTER).grid(row = 0, column = 0, rowspan = 2, padx = 10, pady = 10)
+        ttk.Label(self.frame_header, text = "Welcome to guessN!", style='Header.TLabel').grid(row = 0,column = 1, padx = 10, pady = 10)
+        ttk.Label(self.frame_header, wraplength = 700, text = ("Evaluate your own skill in guessing how many samples " 
+                                                    "you need to record a loss in a population...\n")).grid(row = 1, column = 1, padx = 10)      
+       
         # create a frame to hold the content
         self.frame_content = ttk.Frame(master)
         self.frame_content.pack(side = LEFT)
         
         # assign a label to the user entry field
         ttk.Label(self.frame_content,text = "Enter your guess below...").grid(row = 1, column = 0)
-
-        
+    
         # assign entry field associated with the users guess
         self.entry_guessN = ttk.Entry(self.frame_content, width = 24)
+        self.entry_guessN.bind("<Return>", (lambda e: self.submit()))
+        
         self.text_instructions = Text(self.frame_content, width = 65, height = 8, font = ('Arial', 11))
         self.text_results = Text(self.frame_content, width = 65, height = 10, font = ('Arial', 11))
-
         
         # specify the location of widgets
         self.entry_guessN.grid(row = 2, column = 0, padx = 10, pady = 10)
         self.text_instructions.grid(row=0, column = 0, columnspan = 3,padx = 10, pady = 10)
         self.text_results.grid(row = 4, column = 0, columnspan = 3, padx = 10, pady = 10)
+
         
         # Add buttons for playing and quiting the program
         self.PlayButton =  ttk.Button(self.frame_content, text = "Play",command = self.play)
         self.PlayButton.grid(row = 2, column = 1, padx = 5, pady = 5)
-        self.SubmitButton = ttk.Button(self.frame_content, text = "Submit",command = self.submit, state= DISABLED)
+        self.SubmitButton = ttk.Button(self.frame_content, text = "Submit",command = self.submit, state = DISABLED)
         self.SubmitButton.grid(row = 3, column = 1, padx = 5, pady = 5)
-
-        
-        self.ClearButton = ttk.Button(self.frame_content, text="Clear",command=self.clear)
-        self.ClearButton.grid(row = 2, column = 2, padx = 5,pady = 5)  
+        self.ClearButton = ttk.Button(self.frame_content, text = "Clear",command = self.clear)
+        self.ClearButton.grid(row = 2, column = 2, padx = 5,pady = 5)
         self.ExitButton = ttk.Button(self.frame_content, text="Exit game",command=self.exit_game)
         self.ExitButton.grid(row = 3, column = 2, padx = 5, pady = 5)
-        
+
+   
         # Create plotting frame
         self.frame_plot = ttk.Frame(master)
         self.frame_plot.pack(side = RIGHT)
-        
-        # add a temporary place holder for the figure
-        self.temp_img = PhotoImage(file = "images/logo.gif").subsample(2, 2)
-        ttk.Label(self.frame_plot, image = self.temp_img, anchor=CENTER).grid(row = 0, column = 0, padx = 10, pady = 10)
-        
-   
+
+        # Add a canvas for plotting
+        self.f = Figure(figsize=(6,6), dpi = 50)
+        self.a = self.f.add_subplot(1, 1, 1)
+        self.a.hist(self.error_data, bins = 15)
+
+        self.canvas = FigureCanvasTkAgg(self.f, master)
+        self.canvas.show()
+        self.canvas.get_tk_widget().pack()
+        self.canvas._tkcanvas.pack(padx = 15, pady = 10)     
+
+
     def play(self):
         """Assigns a mean population size (mu) sd and the percentage of loss to game variables, 
         and calculates the new mean (after loss) and the required N to realiably detect the loss."""
@@ -90,8 +96,8 @@ class guessN:
         self.n = ((self.alpha + self.beta) / ((self.mu2 - self.mu1) / self.sd))**2
 
         # clear all fields        
-        self.clear()        
-        #
+        self.clear()
+        
         # Add instruction text to the first text window
         self.instruction_message = ("If you have a population mean of %s and a standard deviation of %s,"
                                     "\nwhat sample size is required to detect a loss of %s percent? \n"
@@ -99,9 +105,10 @@ class guessN:
         
         self.text_instructions.insert(0.0, self.instruction_message)
         #
-        # set submit button in normal state
+        # change button states to guide the player
+        self.PlayButton.state(["disabled"])
         self.SubmitButton.state(["!disabled"])
-
+        self.entry_guessN.state(["!disabled"])
       
     def submit(self):
         """        
@@ -120,52 +127,72 @@ class guessN:
             # if statements are adjusted to be within 1 sampling unit of the correct answer
             if int(content) <= (self.n - 1):
                 diff = self.n - int(content)
+                self.error_data.append(int(content) - self.n)
                 self.Guess_ID += 1
-                self.guess.append(content)
-                self.calculated_N.append(self.n)
+                #self.guess.append(content)
+                #self.calculated_N.append(self.n)
                 message = "Guess # " + str(self.Guess_ID) + ": " + "You have underestimated the number of samples by " + str(int(math.ceil(diff))) + "\nYou would need " + str(int(math.ceil(self.n))) + " samples to detect a loss of " + str(int(math.ceil(self.loss))) + "%"      
                
             elif int(content) >= (self.n + 1):
                 diff = int(content) - (self.n)
+                self.error_data.append(int(content) - self.n)
                 self.Guess_ID += 1
-                self.guess.append(content)
-                self.calculated_N.append(self.n)
+                #self.guess.append(content)
+                #self.calculated_N.append(self.n)
                 message = "Guess # " + str(self.Guess_ID) + ": " + "You have overestimated the number of samples by " + str(int(math.ceil(diff))) + "\nYou need " + str(int(math.ceil(self.n))) + " samples to detect a loss of " + str(int(math.ceil(self.loss))) + "%" 
                 
             else: 
                 diff = 0.0
                 self.Guess_ID += 1
-                self.guess.append(content)
-                self.calculated_N.append(self.n)
+                self.error_data.append(0)
+                #self.guess.append(content)
+                #self.calculated_N.append(self.n)
                 message = "Correct"
-                
+
+            # append the data for plotting
+            self.a.hist(self.error_data, bins = 15, color = "cornflowerblue")
+            self.canvas.show()
+        
+
+            # provide message    
             self.text_results.delete(1.0, 'end')
             self.text_results.insert(0.0, message)
 
-          
+            # change button states to guide the player
+            self.entry_guessN.delete(0, END)
+            self.SubmitButton.state(["disabled"])
+            self.PlayButton.state(["!disabled"])
+            self.entry_guessN.state(["disabled"])
+             
         except:
             self.text_results.delete(1.0, 'end')
             self.exception_message = 'Opps! Something went wrong. Try entering an integer \nor floating point number'
             self.text_results.insert(0.0, self.exception_message)
-
+    
             
     def clear(self):
         """
         The clear function removes all entries from the fields
         """
-        self.entry_guessN.delete(0, 'end')
+        
+        # clear entry text field
+        self.entry_guessN.delete(0, END)
+        
         self.text_instructions.delete(1.0, 'end')
         self.text_results.delete(1.0, 'end')
 
-        # Set submit button to disabled
-        # make sure the play button is enabled (but mostly a redundant step)
+        # change button states to guide the player
         self.SubmitButton.state(["disabled"])
         self.PlayButton.state(["!disabled"])
+
         
-    def exit_game(self): 
+    def exit_game(self):
+        """
+        Exit the game.
+        """        
         root.destroy()        
   
 root = Tk()
 app = guessN(root)
-root.title("guessN! (GUI) v0.1 by RayBlick")
+root.title("guessN! (GUI) v0.2 by RayBlick")
 root.mainloop()
